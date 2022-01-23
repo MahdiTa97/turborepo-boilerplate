09bc523a0a57f77ccd0f8293a00f44cb4094d250
mahdita97 <taala.mahdy@gmail.com>
2022-01-23 16:38:21 +0330

    fix: change my-theme-kit dev script
    and some changes in directory of tailwind css cofig file

--- packages/my-theme-kit/.storybook/preview.js
+++ packages/my-theme-kit/.storybook/preview.js
@@ -1,5 +1,5 @@
 // https://storybook.js.org/docs/react/writing-stories/parameters#global-parameters
-import '../src/assets/tailwind.css';
+import '../tailwind.css';
 
 export const parameters = {
   // https://storybook.js.org/docs/react/essentials/actions#automatically-matching-args
